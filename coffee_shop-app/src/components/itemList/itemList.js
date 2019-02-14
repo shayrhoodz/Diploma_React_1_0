99981925da88c469606838724b8fe7833451c5e9
Radik Shaymardanov <kaddafi88@gmail.com>
2019-02-14 23:27:41 +0500

    Добавил вывод ошибки

--- coffee_shop-app/src/components/itemList/itemList.js
+++ coffee_shop-app/src/components/itemList/itemList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
-import Spinner from '../spinner/spinner'
+import Spinner from '../spinner/spinner';
+import ErrorMessage from '../errorMessage/';
 
 class ItemList extends Component {
   
@@ -10,6 +11,14 @@ class ItemList extends Component {
     // term: '',
     error: false,
     loading: true
+  }
+
+  onError = (err) => {
+    // console.log(err);
+    this.setState({
+      error: true,
+      loading: false
+    })
   }
   
   componentDidMount() {
@@ -25,6 +34,9 @@ class ItemList extends Component {
                   loading: false            
               })
           })
+          .catch(this.onError);
+          console.log(this.state.itemList);
+          console.log(this.onError);
   }
 
   renderCoffee(arr) {
@@ -83,12 +95,16 @@ class ItemList extends Component {
 
   render() {
 
-    const {itemList} = this.state;
+    const {itemList, error} = this.state;
     const {term, filter} = this.props;
+    console.log(error);
     
-    if (!itemList) {
+    if (itemList === '') {
         return null
     }
+    // if (!itemList) {
+    //     return null
+    // }
 
     // if (error) {
     //     return <ErrorMessage/>
@@ -97,9 +113,11 @@ class ItemList extends Component {
     const resFilter = this.filterPost(this.updateData(itemList, term), filter);
     // console.log(itemList, term);
     const coffee = this.renderCoffee(resFilter);       
+    const errorMessage = error ? <ErrorMessage /> : null
 
     return (
       <>
+        {errorMessage}
         {coffee}
       </>
     );
